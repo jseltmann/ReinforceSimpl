10ce08ac251ba24199c33ed9a76408f3388566a6
Johann Seltmann <jseltmann@uni-potsdam.de>
2020-08-11 19:44:41 +0000

    load wikipedia aligned sentences

--- load_data.py
+++ load_data.py
@@ -30,19 +30,44 @@ def split_wiki_sent(data_dir, split_dir):
     train = zipped[:cutoff]
     test = zipped[cutoff:]
 
-    normal_path = os.path.join(split_dir, "normal.train.aligned")
-    simple_path = os.path.join(split_dir, "simple.train.aligned")
+    normal_path = os.path.join(split_dir, "train/normal.aligned")
+    simple_path = os.path.join(split_dir, "train/simple.aligned")
     with open(normal_path, "w") as nfile, open(simple_path, "w") as sfile:
         for nline, sline in train:
             nfile.write(nline)
             sfile.write(sline)
 
-    normal_path = os.path.join(split_dir, "normal.test.aligned")
-    simple_path = os.path.join(split_dir, "simple.test.aligned")
+    normal_path = os.path.join(split_dir, "test/normal.aligned")
+    simple_path = os.path.join(split_dir, "test/simple.aligned")
     with open(normal_path, "w") as nfile, open(simple_path, "w") as sfile:
         for nline, sline in test:
             nfile.write(nline)
             sfile.write(sline)
 
-#split_wiki_sent("/data/sentence-aligned.v2",
-#                "/data/wiki/sent_aligned_split")
+split_wiki_sent("/data/sentence-aligned.v2",
+                "/data/wiki/sent_aligned_split")
+
+
+def load_wiki_sents(data_dir):
+    """
+    Load sentence pairs from wikipedia set.
+
+    Parameters
+    ----------
+    data_dir : str
+        Directory containing the files with
+        aligned normal and simple sentences.
+    """
+
+    normal_path = os.path.join(data_dir, "normal.aligned")
+    simple_path = os.path.join(data_dir, "simple.aligned")
+
+    with open(normal_path) as nfile:
+        lines = nfile.readlines()
+        nsents = [l.split("\t")[2].strip() for l in lines]
+    with open(simple_path) as sfile:
+        lines = sfile.readlines()
+        ssents = [l.split("\t")[2].strip() for l in lines]
+
+    pairs = list(zip(nsents, ssents))
+    return pairs
