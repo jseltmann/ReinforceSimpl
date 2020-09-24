de5ebfcdd3b048710ecabe021d65adfc94210200
Johann Seltmann <jseltmann@uni-potsdam.de>
2020-09-24 08:46:48 +0000

    supervised transformer training

--- train.py
+++ train.py
@@ -1,12 +1,18 @@
 import transformers as tr
 import torch
+import torch.nn as nn
 from tqdm import tqdm
 import math
+from random import random
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
 
 import load_data as ld
-from policy import Policy
-from rewards import bleu_reward, sari_reward
+from policy import Policy, GPT2Policy
+from rewards import bleu_reward, sari_reward, simple_english_reward
 
+#import warnings
+#warnings.simplefilter("ignore", message="Set")
 
 def batchify(data, batch_size):
     """
@@ -33,7 +39,7 @@ def batchify(data, batch_size):
     return batched
 
 
-def train_base(data, reward_fn, model_save_path, device="cpu", epochs=1):
+def train_base(data, reward_fn, model_save_path, logdir, device="cpu", epochs=1):
     """
     Train model.
 
@@ -47,6 +53,8 @@ def train_base(data, reward_fn, model_save_path, device="cpu", epochs=1):
         the gold sentence and produces an reward score.
     model_save_path : str
         Path to save trained model to.
+    logdir : str
+        Path to save tensorboard logs to.
     device : str
         "cpu" or "cuda".
     """
@@ -55,12 +63,14 @@ def train_base(data, reward_fn, model_save_path, device="cpu", epochs=1):
     policy.train()
     policy.to(device)
 
-    optimizer = torch.optim.Adam(policy.parameters(), lr=1e-2)
+    optimizer = torch.optim.AdamW(policy.parameters(), lr=5e-5)
+    writer = SummaryWriter(logdir)
 
     batches = batchify(data, 8)
 
     loss0 = torch.tensor(0.).to(device)
 
+    i = 0
     for epoch in range(epochs):
         for batch in tqdm(batches):
             normal_batch = [pair[0] for pair in batch]
@@ -74,18 +84,94 @@ def train_base(data, reward_fn, model_save_path, device="cpu", epochs=1):
             for sampled, simple, orig in zip(sampled_sents, simple_batch, normal_batch):
                 reward = reward_fn(sampled, simple, orig)
                 rewards.append(reward)
+            writer.add_scalar("Reward mean", np.mean(rewards), i)
+            writer.add_scalar("Reward std dev", np.std(rewards), i)
 
             loss = loss0.detach().clone()
             for reward, sent_prob in zip(rewards, sent_probs):
                 loss -= reward * torch.log(sent_prob)
+            writer.add_scalar("Loss", loss.item(), i)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            i += 1
+        torch.save(policy.state_dict(), model_save_path+str(epoch))
 
     torch.save(policy.state_dict(), model_save_path)
+    #close_client()
 
 
-data = ld.load_wiki_sents("/data/data/wiki/sent_aligned_split/train")
-train_base(data, bleu_reward, "/data/tuned_models/bleu/bleu_5ep.pt", device="cuda", epochs=5)
-#train_base(data, sari_reward, "/data/tuned_models/sari/sari_full.pt", device="cuda")
+def train_supervised(data, model_save_path, device="cpu", epochs=1):
+    """
+    Train model using normal supervised training.
+
+    Parameters
+    ----------
+    data : [(str,str)]
+        Training pairs of normal and 
+        simplified sentences or texts.
+    model_save_path : str
+        Path to save trained model to.
+    device : str
+        "cpu" or "cuda".
+    """
+
+
+    tokenizer = tr.BartTokenizer.from_pretrained("facebook/bart-base")
+    #tokenizer = tr.BertTokenizer.from_pretrained("bert-base-uncased")
+    #tokenizer = tr.GPT2Tokenizer.from_pretrained("gpt2")
+    #tokenizer.pad_token = tokenizer.eos_token
+    writer = SummaryWriter("/data/tensorboard/supervised/30ep")
+
+    batches = batchify(data, 8)
+
+    data_ids = []
+    for batch in tqdm(batches):
+        normal_batch = [pair[0] for pair in batch]
+        simple_batch = [pair[1] for pair in batch]
+        normal_ids = tokenizer(normal_batch, padding='max_length', return_tensors='pt', max_length=70, truncation=True)
+        simple_ids = tokenizer(simple_batch, padding='max_length', return_tensors='pt', max_length=70, truncation=True)
+        data_ids.append((normal_ids, simple_ids))
+
+    model = tr.BartForConditionalGeneration.from_pretrained("facebook/bart-base")
+    #model = tr.GPT2LMHeadModel.from_pretrained("gpt2")
+    #config = tr.GPT2Config.from_pretrained("gpt2")
+    #model = tr.GPT2LMHeadModel(config)
+    #config = tr.BartConfig.from_pretrained("facebook/bart-base")
+    #model = tr.BartForConditionalGeneration(config)
+    model.to(device)
+    model.train()
+    #optimizer = tr.AdamW(model.parameters(), lr=5e-5)
+    optimizer = tr.AdamW(model.parameters(), lr=1e-4)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id, reduction='mean')
+
+    i = 0
+    for epoch in tqdm(range(epochs)):
+        #print(epoch)
+        #for batch in tqdm(data_ids):
+        for batch in data_ids:
+            normal_batch = batch[0].to(device)
+            simple_batch = batch[1].to(device)
+
+            outputs = model(**normal_batch)
+            generated = outputs[0]
+            sums = generated.sum(2)
+            generated = generated.permute(0,2,1)
+            loss = loss_fn(generated, simple_batch["input_ids"])
+            writer.add_scalar("Loss", loss.item(), i)
+            i += 1
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        torch.save(model.state_dict(), model_save_path+str(epoch))
+
+    torch.save(model.state_dict(), model_save_path)
+
+
+#data = ld.load_wiki_sents("/data/data/wiki/sent_aligned_split/train")
+data = ld.load_newsela_sents("/data/data/newsela/V0V1/train")
+#train_base(data, simple_english_reward, "/data/tuned_models/bleu/bleu_newsela.pt", "/data/tensorboard/bleu/bleu_newsela", device="cuda", epochs=10)
+#train_base(data, simple_english_reward, "/data/tuned_models/ser/bart_ser.pt", "/data/tensorboard/ser/ser_test", device="cuda", epochs=1)
+train_supervised(data, "/data/tuned_models/supervised/bart_newsela.pt", device="cuda", epochs=20)
